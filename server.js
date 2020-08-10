32b3293088a30af5813ef7dfcc82a0361bcd52ed
Germán Téllez <gertellezv@gmail.com>
2020-08-10 02:34:30 -0500

    Preparing deploy

--- server.js
+++ server.js
@@ -4,19 +4,25 @@ const router = require("./network/routes");
 const db = require("./db");
 
 var app = express();
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 );
-const url_db = "mongodb+srv://db_user_germantellezv:j0U8PJgdXILb@cluster0.l7nqi.mongodb.net/jobs_db?retryWrites=true&w=majority";
 
+// DB
+const url_db =
+  "mongodb+srv://db_user_germantellezv:j0U8PJgdXILb@cluster0.l7nqi.mongodb.net/jobs_db?retryWrites=true&w=majority";
 db(url_db);
 
 router(app);
 
+// Static files
 app.use("/", express.static("public"));
 
-app.listen(3003);
-console.log("La aplicación está escuchando en el puerto 3003");
+// Run server
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`La aplicación está escuchando en el puerto ${port}`);
