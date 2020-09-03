39451bcb2a70e7f2ef8b44d9453ecc3aa06731b3
Germán Téllez <gertellezv@gmail.com>
2020-09-03 04:24:09 -0500

    API Docs

--- network/routes.js
+++ network/routes.js
@@ -1,9 +1,13 @@
 const jobs = require("../components/jobs/network");
 const home = require("../components/home/network");
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDoc = require('../swagger.json')
+
 const routes = function (app) {
   app.use("/", home);
   app.use("/jobs", jobs);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 };
 
 module.exports = routes;
